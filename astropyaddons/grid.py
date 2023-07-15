706e1a8cb88bb774f57bfd14b845e2f99e09ff45
Phy9 <yangjie.linhu@prismsus.org>
2023-07-15 15:28:28 +0200

    Renaming of folder name

--- astropyaddons/grid.py
+++ astropyaddons/grid.py
@@ -24,15 +24,32 @@ class Grid:
             for y in range(size_y)
         }
 
+    def as_numpy_array(self) -> np.ndarray[np.ndarray[float]]:
+        data = [
+            [self.pixels[Coordinates(x,y)].counts for x in range(self.size_x)]
+            for y in range(self.size_y)
+        ]
+        return np.array(data)
+
     @property
     def pixel_values(self) -> list[int]:
         values = [pixel.counts for pixel in self.pixels.values()]
         return values
 
     @property
+    def std(self) -> float:
+        return np.std(self.pixel_values)
+
+    @property
+    def mean(self) -> float:
+        return np.mean(self.pixel_values)
+
+    @property
     def median(self) -> float:
         return np.median(self.pixel_values)
-    
+
+
+
 class DataGrid(Grid):
     """
     Grid of pixels made from a 2-D numpy array.
@@ -40,8 +57,15 @@ class DataGrid(Grid):
     Parameters:
      - `array`: A 2-D numpy array to make a Grid object from.
     """
-    def __init__(self, array: np.ndarray[np.ndarray]):
+    def __init__(self, array: np.ndarray[np.ndarray[float]]):
         if not isinstance(array, np.ndarray): raise TypeError("'array' must be a numpy array")
         if array.ndim != 2: raise ValueError("'array' must be 2-dimensional")
 
-        size_y, size_y = np.shape(array)
+        size_y, size_x = np.shape(array)
+
+        super().__init__(size_x, size_y)
+
+        for y, row in enumerate(array):
+            for x, counts in enumerate(row):
+                self.pixels[Coordinates(x, y)] += counts
+            
