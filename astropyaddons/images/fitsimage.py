03c0d35e610a054541b5445348bbaa1b534c0032
Phy9 <yangjie.linhu@prismsus.org>
2023-07-17 19:55:14 +0200

    Quickplot + updating names.

--- astropyaddons/images/fitsimage.py
+++ astropyaddons/images/fitsimage.py
@@ -1,4 +1,5 @@
 from astropy.io import fits
+import matplotlib.pyplot as plt
 
 from ..grid import Grid
 from .header import Header
@@ -9,29 +10,45 @@ class FITSImage:
     Class to handle FITS images. Load with `FITSImage(filepath)`.
     """
 
-    def __init__(self, filepath, image_to_load: int=None):
+    def __init__(self, filepath, id: int=None):
         """
         Parameters:
          - `filepath`: filepath of FITS image to load.
-         - `image_to_load`: which image to load of the file.
+         - `id`: which image to load of the file (index).
             If none specified, will load the first image.
         """
 
         # LOAD FITS FILE
-        with fits.open(filepath) as fits_image:
-            if image_to_load is None:
-                image_to_load = 0
-                if len(fits_image) != 1:
-                    print(f"Warning: FITS image with filepath {self.fp}"\
-                            "has multiple images."\
-                            "Only the first one will be loaded.")
+        with fits.open(filepath) as images:
+            if id is None:
+                id = 0
+                if len(images) != 1:
+                    print(f"No image id specified for FITS image {filepath}."\
+                        "Only the first one will be loaded.")
             
             # Extract useful information from the fits image.
-            self.header: Header = Header(fits_image[image_to_load].header)
-            self.grid: Grid = Grid(fits_image[image_to_load].data)
+            self.header: Header = Header(images[id].header)
+            self.grid: Grid = Grid(images[id].data)
 
         # Establish the WCS.
         self.wcs: WCS = WCS(self.header)
 
     def __repr__(self):
-        return f'FITSImage of [{self.filter}, {self.date}, {self.time}, exptime {self.exptime}s]'
+        return f'FITSImage of [{self.header.filter}, {self.header.datetime}, exptime {self.header.exptime}s]'
+    
+    def quickplot(self, phi_lo: float=-1, phi_hi: float=3) -> None:
+        """
+        Displays a simple grayscale plot of the data. 
+        
+        Parameters:
+          - `lo_phi`: standard deviations from median to assign to black
+          - `hi_phi`: standard deviations from median to assign to white
+        """
+        assert phi_lo < phi_hi, "lo_phi must be lower than hi_phi"
+
+        lo = self.grid.median + phi_lo*self.grid.std
+        hi = self.grid.median + phi_hi*self.grid.std
+
+        plt.figure(figsize=(10,10))
+        plt.title(self.__repr__())
+        plt.imshow(self.grid.grid, cmap='gray', origin='lower', vmin=lo, vmax=hi)
