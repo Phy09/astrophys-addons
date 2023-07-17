538a776139a7796a48924e535754775cea6866b1
Phy9 <yangjie.linhu@prismsus.org>
2023-07-17 13:38:35 +0200

    Cleaning
    Cleaning bugs, updating values, and adding a "Max" to the moffat function so that it is easier to fit.

--- astropyaddons/PSF/psf.py
+++ astropyaddons/PSF/psf.py
@@ -1,36 +1,39 @@
 from typing import Callable
 import numpy as np
 from scipy import integrate
+from abc import ABC, abstractmethod
 
-from ..coordinates import Coordinates
+coord = tuple[float, float]
 
-class PSF:
+class PSF(ABC):
     """
-    Point Spread Function. 
+    Abstract Class of Point Spread Function. 
     Used to calculate the counts each pixel on a CCD would have.
-
-    Parameters:
-     - `function`: Callable function with one input argument,
-        that being the distance from the center of the PSF.
     """
+    @property
+    @abstractmethod
+    def function(self):
+        pass
 
-    def __init__(self, function: Callable):
-        self.function: Callable = function
-
-    def counts_pixel(self, center: Coordinates, pixel: Coordinates, pixel_size: int or float = 1):
+    def counts_pixel(self, center: coord, pixel: coord, pixel_size: tuple=(1,1)):
         """
         Function to get the counts that a pixel on the CCD would have.
         The double integral of the PSF is calculated for the pixel in question.
-         - `center`: `Coordinates` object of the point light source.
-         - `pixel`: `Coordaintes` object of the pixel which we want the counts of.
-         - `pixel_size`: Size of the pixel, assumed to be 1 (representing 1 pixel) 
+         - `center`: tuple of the point light source.
+         - `pixel`: tuple of the pixel's coordinates which we want the counts of.
+         - `pixel_size`: Size of the pixel, assumed to be 1 in both directions
+           (representing 1 pixel) 
         """
         radius_function = lambda x, y: self.function((x*x + y*y) ** .5)
 
+        centerx, centery = center
+        pixelx, pixely = pixel
+        sizex, sizey = pixel_size
+
         integral, error = integrate.dblquad(
             radius_function,
-            pixel.y-center.y, pixel.y-center.y+pixel_size, # BOUNDS FOR Y COORDINATE
-            lambda _: pixel.x-center.x, lambda _: pixel.x-center.x+pixel_size, # BOUNDS FOR X COORDINATE
+                        pixely-centery,             pixely-centery+sizey, # BOUNDS FOR Y COORDINATE
+            lambda _:   pixelx-centerx, lambda _:   pixelx-centerx+sizex, # BOUNDS FOR X COORDINATE
         )
 
         return integral
@@ -38,37 +41,42 @@ class PSF:
 
 
 # GAUSSIAN FUNCTION
-def gaussian(x, sd: float, max: float):
+def gaussian(r, std: float, max: float):
     """
     Returns the value of the gaussian function:
-     - `distance`: Distance to mean of gaussian curve
-     - `sd`: Standard Deviation of gaussian curve
+     - `r`: Distance to mean of gaussian curve
+     - `std`: Standard Deviation of gaussian curve
      - `max`: Maximum of gaussian curve at mean
     """
-    return (np.e**(-.5 * ((x)/sd)**2) * max)
+    return max * np.e**(-.5 * ((r)/std)**2)
 class GaussianPSF(PSF):
     """
     Point Spread function using the Gaussian equation.
     Parameters:
-     - `sd`: Standard deviation of gaussian curve.
+     - `std`: Standard deviation of gaussian curve.
      - `max`: Max value of gaussian curve at mean.
     """
 
-    def __init__(self, sd: float, max: float):
-        self.alpha: float = sd
-        self.beta: float = max
-    
+    def __init__(self, std: float, max: float):
+        self.std: float = std
+        self.max: float = max
+
     @property
     def function(self):
-        return lambda r: moffat(r, self.alpha, self.beta)
+        return lambda r: moffat(r, self.std, self.max)
 
 
 
 # MOFFAT FUNCTION
 # https://www.ias.ac.in/article/fulltext/joaa/009/01/0017-0024
-def moffat(x, alpha: float, beta: float):
-    F_0 = (beta - 1) * np.pi * alpha**2
-    return F_0 * (1 + ((x)/alpha)**2) ** (-beta)
+def moffat(r, alpha: float, beta: float, max: float):
+    """
+    Returns the value of the moffat distribution:
+     - `r`: Distance to mean of gaussian curve
+     - `alpha`, `beta`: Alpha and beta of the Moffat distribution.
+     - `max`: Max value at mean of the distribution.
+    """
+    return max * (1 + ((r)/alpha)**2) ** (-beta)
 class MoffatPSF(PSF):
     """
     Point Spread function using the Moffat distribution.
@@ -76,10 +84,11 @@ class MoffatPSF(PSF):
      - `alpha`, `beta`: Alpha and beta of the standard Moffat distribution.
     """
 
-    def __init__(self, alpha: float, beta: float):
+    def __init__(self, alpha: float, beta: float, max: float):
         self.alpha: float = alpha
         self.beta: float = beta
-     
+        self.max: float = max
+
     @property
     def function(self):
-        return lambda r: moffat(r, self.alpha, self.beta)
+        return lambda r: moffat(r, self.alpha, self.beta, self.max)
